04d6b49c16596cbd7cf006336d9fe97304ba66c3
dmpayton <derek@cukerinteractive.com>
2010-12-14 16:45:00 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+setup(name="TRML2PDF",
+      version="1.0",
+      author="Fabien Pinckaers",
+      author_email="fp@tiny.be",
+      license="LGPL",
+      description="Tiny RML2PDF is a tool to easily create PDF document without programming.",
+      packages = ['trml2pdf'],
+      package_dir = {'trml2pdf': 'trml2pdf'}
+     )
